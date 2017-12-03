7cc7baa67b30ccc94f3f9361347c02bae8e1e863
Goncharov Stepan <gsv28rus@gmail.com>
2017-12-03 14:06:47 +0900

    03.12.2017 - добавлен ToolBar в Activity.

--- app/src/main/java/com/gsv28rus/android/addictfoodnumber/AddictNumberActivity.java
+++ app/src/main/java/com/gsv28rus/android/addictfoodnumber/AddictNumberActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -16,6 +17,8 @@ public class AddictNumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addict_number_list);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
